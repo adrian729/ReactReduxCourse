ec319d8c4121045d0d974110a90f8e567e1c1b73
adrian.sanchez.albanell <adrian.sanchez.albanell@fib.upc.edu>
2018-10-11 16:42:45 +0200

    finish  middlewares

--- middlewares/src/middlewares/async.js
+++ middlewares/src/middlewares/async.js
@@ -1,3 +1,13 @@
 export default ({ dispatch }) => next => action => {
-
-}
+    // Check if promise on action payload
+    
+    // If not, send to next
+    if(!action.payload || !action.payload.then) {
+        return next(action);
+    }
+    // If, wait for it to resolve, create a new action with that data and dispatch it
+    action.payload.then(function (response) {
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+    });
+};
