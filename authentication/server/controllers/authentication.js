03c69a22cc8b368f552fec581d457c9d3df78242
adrian.sanchez.albanell <adrian.sanchez.albanell@fib.upc.edu>
2018-10-15 15:49:53 +0200

    end auth server, init auth client

--- authentication/server/controllers/authentication.js
+++ authentication/server/controllers/authentication.js
@@ -1,15 +1,28 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
 
-exports.signup = function(req, res, next) {
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function (req, res, next) {
+    // User has already email and password auth'd
+    // Give them a token
+    res.send({ token: tokenForUser(req.user) });
+}
+
+exports.signup = function (req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
-    if (!email || ! password) {
+    if (!email || !password) {
         return res.status(422).send({ error: 'You must provide email and password' });
     }
 
     // See if an user with the given email exists
-    User.findOne({ email: email }, function(err, existingUser) {
+    User.findOne({ email: email }, function (err, existingUser) {
         if (err) { return next(err); }
 
         // If exists, return error
@@ -22,11 +35,11 @@ exports.signup = function(req, res, next) {
             password: password
         });
 
-        user.save(function(err){
-            if(err) { return next(err); }
+        user.save(function (err) {
+            if (err) { return next(err); }
 
             // Respond to request indicating the user was created
-            res.json({ success: true });
+            res.json({ token: tokenForUser(user) });
         });
     });
 }
